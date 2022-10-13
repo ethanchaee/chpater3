d0831f574bc7e7d2d0856ba0067836a6459a9b83
ethanchae <boschae36@gmail.com>
2022-10-13 22:24:58 +0900

    feat : UI 에서 데이터 추출해서 전송

--- app/src/main/java/fastcampus/aop/part1/chpater3/InputActivity.kt
+++ app/src/main/java/fastcampus/aop/part1/chpater3/InputActivity.kt
@@ -1,8 +1,10 @@
 package fastcampus.aop.part1.chpater3
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
 import fastcampus.aop.part1.chpater3.databinding.ActivityInputBinding
 
 class InputActivity : AppCompatActivity() {
@@ -12,11 +14,43 @@ class InputActivity : AppCompatActivity() {
         binding = ActivityInputBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.completeButton.setOnClickListener {
+        binding.bloodTypeSpinner.adapter = ArrayAdapter.createFromResource(
+            this,
+            R.array.blood_type_alphabet_array,
+            android.R.layout.simple_list_item_1
+        )
+
+        binding.checkBox.setOnCheckedChangeListener { _, isChecked ->
+            binding.editTextWarning.isVisible = isChecked
+        }
+
+        binding.textViewComplete.setOnClickListener {
             Intent(this, MainActivity::class.java).apply {
                 putExtra("name", binding.editTextName.text.toString())
+                putExtra("bloodType", getBloodType())
+                putExtra("phoneNumber", binding.editTextPhone.text.toString())
+                putExtra("warning", getWarning())
                 startActivity(this)
             }
         }
     }
+
+    private fun getBloodType(): String {
+        val bloodTypeSign = if (binding.radioGroup.checkedRadioButtonId == R.id.radioButtonPlus) {
+            "+"
+        } else {
+            "-"
+        }
+
+        val bloodTypeAlphabet = binding.bloodTypeSpinner.selectedItem
+
+        return "$bloodTypeSign$bloodTypeAlphabet"
+    }
+
+    private fun getWarning() =
+        if (binding.checkBox.isSelected) {
+            binding.editTextWarning.toString()
+        } else {
+            ""
+        }
 }
